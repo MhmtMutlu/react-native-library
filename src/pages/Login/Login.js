df1ce0c51f970557209d657a7a2ada2ff6353fdf
MhmtMutlu <49007453+MhmetMutlu@users.noreply.github.com>
2022-05-03 17:10:51 +0300

    UI: Login page was created

---
+++ src/pages/Login/Login.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+const Login = () => {
+  return (
+    <View>
+      <Text>My Library Login</Text>
+    </View>
+  );
+};
+
+export default Login;
