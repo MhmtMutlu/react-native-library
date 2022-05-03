44301ac1e209b7c771d6f8a859f5ef6aa2afa902
MhmtMutlu <49007453+MhmetMutlu@users.noreply.github.com>
2022-05-03 17:01:25 +0300

    Init: Project was created

---
+++ src/Router.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+const Router = () => {
+  return (
+    <View>
+      <Text>My Library</Text>
+    </View>
+  );
+};
+
+export default Router;
