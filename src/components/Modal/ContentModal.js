a9a205e2e6169437a71c5e58b3727038b5f3a552
MhmtMutlu <49007453+MhmetMutlu@users.noreply.github.com>
2022-05-12 23:57:18 +0300

    UI-Feat: ContentModal was edited

--- src/components/Modal/ContentModal.js
+++ src/components/Modal/ContentModal.js
@@ -3,18 +3,20 @@ import {View, TextInput} from 'react-native';
 import Button from '../Button';
 import Modal from 'react-native-modal';
 import styles from './ContentModal.styles';
+import {Formik} from 'formik';
+
+const initialValues = {
+  bookName: '',
+  writer: '',
+  bookSubject: '',
+  bookPages: 0,
+  quotes: '',
+  startDate: '',
+  endDate: '',
+  imageUrl: '',
+};
 
 const ContentModal = ({isVisible, onSend, onClose}) => {
-  const [text, setText] = React.useState(null);
-
-  const handleSend = () => {
-    if (!text) {
-      return;
-    }
-    onSend(text);
-    setText(null);
-  };
-
   return (
     <Modal
       style={styles.modal}
@@ -24,10 +26,69 @@ const ContentModal = ({isVisible, onSend, onClose}) => {
       onBackdropPress={onClose}
       onBackButtonPress={onClose}>
       <View style={styles.container}>
-        <View style={styles.input}>
-          <TextInput multiline placeholder="deneme" onChangeText={setText} />
-        </View>
-        <Button text="Gönder" onPress={handleSend} />
+        <Formik initialValues={initialValues} onSubmit={onSend}>
+          {({handleSubmit, handleChange, values}) => (
+            <View style={styles.body_container}>
+              <View style={styles.input}>
+                <TextInput
+                  value={values.bookName}
+                  placeholder="Kitap adı ..."
+                  onChangeText={handleChange('bookName')}
+                />
+              </View>
+              <View style={styles.input}>
+                <TextInput
+                  value={values.writer}
+                  placeholder="Yazarı ..."
+                  onChangeText={handleChange('writer')}
+                />
+              </View>
+              <View style={styles.input}>
+                <TextInput
+                  value={values.bookSubject}
+                  placeholder="Kitabın konusu ..."
+                  onChangeText={handleChange('bookSubject')}
+                />
+              </View>
+              <View style={styles.input}>
+                <TextInput
+                  value={values.quotes}
+                  placeholder="Alıntılar ..."
+                  onChangeText={handleChange('quotes')}
+                />
+              </View>
+              <View style={styles.input}>
+                <TextInput
+                  value={values.startDate}
+                  placeholder="Başlangıç tarihi ..."
+                  onChangeText={handleChange('startDate')}
+                />
+              </View>
+              <View style={styles.input}>
+                <TextInput
+                  value={values.endDate}
+                  placeholder="Başlangıç tarihi ..."
+                  onChangeText={handleChange('endDate')}
+                />
+              </View>
+              <View style={styles.input}>
+                <TextInput
+                  value={values.imageUrl}
+                  placeholder="Kitabın resmi ..."
+                  onChangeText={handleChange('imageUrl')}
+                />
+              </View>
+              <View style={styles.button_wrapper}>
+                <Button text="Giriş Yap" onPress={handleSubmit} />
+                <Button
+                  text="Kaydet"
+                  theme="secondary"
+                  onPress={handleSubmit}
+                />
+              </View>
+            </View>
+          )}
+        </Formik>
       </View>
     </Modal>
   );
