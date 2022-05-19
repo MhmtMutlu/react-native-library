98468554241176d818a263a0cefda2c26e176b3c
MhmtMutlu <49007453+MhmetMutlu@users.noreply.github.com>
2022-05-20 00:42:44 +0300

    Feat: Firebase operations were completed for taking and saving data

--- src/pages/Library/Library.js
+++ src/pages/Library/Library.js
@@ -1,14 +1,31 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {FlatList, SafeAreaView} from 'react-native';
+
+import database from '@react-native-firebase/database';
+import auth from '@react-native-firebase/auth';
 
 import Card from '../../components/Card';
 import FloatingButton from '../../components/FloatingButton';
 import ContentModal from '../../components/Modal/ContentModal';
+import parsedData from '../../utils/parsedData';
 
 import styles from './Library.styles';
 
 const Library = () => {
-  const [inputModalVisible, setInputModalVisible] = React.useState(false);
+  const [inputModalVisible, setInputModalVisible] = useState(false);
+  const [contentList, setContentList] = useState([]);
+  const userMail = auth().currentUser.email.replace('.', '');
+  console.log('userMail', userMail);
+
+  useEffect(() => {
+    database()
+      .ref(`${userMail}/`)
+      .on('value', snapshot => {
+        const contentData = snapshot.val();
+        const parsedContentData = parsedData(contentData || {});
+        setContentList(parsedContentData);
+      });
+  }, [userMail]);
 
   const handleModalClose = () => {
     setInputModalVisible(false);
@@ -18,21 +35,30 @@ const Library = () => {
     setInputModalVisible(!inputModalVisible);
   };
 
-  const renderContent = ({item}) => (
-    <Card message={item} onPress={() => null} />
+  const sendContent = content => {
+    const contentObj = {
+      bookName: content.bookName,
+      writer: content.writer,
+      bookSubject: content.bookSubject,
+      startDate: content.startDate.toISOString(),
+      endDate: content.endDate.toISOString(),
+    };
+    database().ref(`${userMail}/`).push(contentObj);
+    handleModalToggle();
+  };
+
+  const renderContent = ({item, index}) => (
+    <Card cardData={item} index={index + 1} onPress={() => null} />
   );
 
   return (
     <SafeAreaView style={styles.container}>
-      <FlatList
-        data={[{}, {}, {}, {}, {}, {}, {}, {}, {}, {}]}
-        renderItem={renderContent}
-      />
+      <FlatList data={contentList} renderItem={renderContent} />
       <FloatingButton icon="plus" onPress={handleModalToggle} />
       <ContentModal
         isVisible={inputModalVisible}
         onClose={handleModalClose}
-        onSend={() => null}
+        onSend={sendContent}
       />
     </SafeAreaView>
   );
