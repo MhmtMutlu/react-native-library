02f1f109fef7a6faecc0421e2724d89f14039e58
MhmtMutlu <49007453+MhmetMutlu@users.noreply.github.com>
2022-05-20 00:42:58 +0300

    UI-Feat: Card component was edited

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -6,22 +6,23 @@ import {formatDistance, parseISO} from 'date-fns';
 
 import styles from './Card.styles';
 
-const Card = ({cardData, onPress}) => {
-  // const formattedDate = formatDistance(parseISO(cardData.date), new Date(), {
-  //   addSuffix: true,
-  //   locale: tr,
-  // });
+const Card = ({cardData, index, onPress}) => {
+  const formattedDate = date =>
+    formatDistance(parseISO(date), new Date(), {
+      addSuffix: true,
+      locale: tr,
+    });
 
   return (
     <TouchableOpacity style={styles.container} onPress={onPress}>
       <View style={styles.left_container}>
         <View style={styles.circle}>
-          <Text style={styles.circle_text}>1</Text>
+          <Text style={styles.circle_text}>{index}</Text>
         </View>
       </View>
       <View style={styles.right_container}>
-        <Text style={styles.title}>Clarissa</Text>
-        <Text style={styles.writer}>Stefan Zweig</Text>
+        <Text style={styles.title}>{cardData.bookName}</Text>
+        <Text style={styles.writer}>{cardData.writer}</Text>
       </View>
     </TouchableOpacity>
   );
