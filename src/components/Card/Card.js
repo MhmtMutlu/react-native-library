cec198c2365d6cdc67d2f4f48672cc12a7f050da
MhmtMutlu <49007453+MhmetMutlu@users.noreply.github.com>
2022-05-23 22:39:23 +0300

    Fix: Card component was cleared

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,18 +1,9 @@
 import React from 'react';
 import {TouchableOpacity, Text, View} from 'react-native';
 
-import {tr} from 'date-fns/locale';
-import {formatDistance, parseISO} from 'date-fns';
-
 import styles from './Card.styles';
 
 const Card = ({cardData, index, onPress}) => {
-  const formattedDate = date =>
-    formatDistance(parseISO(date), new Date(), {
-      addSuffix: true,
-      locale: tr,
-    });
-
   return (
     <TouchableOpacity style={styles.container} onPress={onPress}>
       <View style={styles.left_container}>
